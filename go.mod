7890e1e3254874b6d3398f2f2cfd2f82666b535b
Lao Fung <laofun@gmail.com>
2021-01-15 11:09:25 +0700

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/laofun/maroto-vi-vi
+
+go 1.14
+
+require (
+	github.com/boombuler/barcode v1.0.0
+	github.com/google/uuid v1.1.5
+	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.7.0
+)
---
+++ internal/examples/barcodegrid/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/laofun/maroto-vi-vi/pkg/consts"
+	"github.com/laofun/maroto-vi-vi/pkg/pdf"
+	"github.com/laofun/maroto-vi-vi/pkg/props"
+)
+
+func main() {
+	begin := time.Now()
+	m := pdf.NewMaroto(consts.Portrait, consts.Letter)
+	m.SetBorder(true)
+
+	m.Row(40, func() {
+		m.Col(2, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Percent: 50,
+			})
+		})
+		m.Col(4, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Percent: 75,
+			})
+		})
+		m.Col(6, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Percent: 100,
+			})
+		})
+	})
+
+	m.Row(40, func() {
+		m.Col(2, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Center:  true,
+				Percent: 50,
+			})
+		})
+		m.Col(4, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Center:  true,
+				Percent: 75,
+			})
+		})
+		m.Col(6, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Center:  true,
+				Percent: 100,
+			})
+		})
+	})
+
+	m.Row(40, func() {
+		m.Col(6, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Percent: 50,
+			})
+		})
+		m.Col(4, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Percent: 75,
+			})
+		})
+		m.Col(2, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Percent: 100,
+			})
+		})
+	})
+
+	m.Row(40, func() {
+		m.Col(6, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Center:  true,
+				Percent: 50,
+			})
+		})
+		m.Col(4, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Center:  true,
+				Percent: 75,
+			})
+		})
+		m.Col(2, func() {
+			m.Barcode("https://github.com/laofun/maroto-vi-vi", props.Barcode{
+				Center:  true,
+				Percent: 100,
+			})
+		})
+	})
+
+	err := m.OutputFileAndClose("internal/examples/pdfs/barcodegrid.pdf")
+	if err != nil {
+		fmt.Println("Could not save PDF:", err)
+		os.Exit(1)
+	}
+
+	end := time.Now()
+	fmt.Println(end.Sub(begin))
+}
